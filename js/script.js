6c305b497f72cf6cbc7cf297737d5d734ceb3175
TungstenEagle <67823618+TungstenEagle@users.noreply.github.com>
2020-08-20 20:51:24 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -39,6 +39,44 @@ $('.click_back').each(function(i) {
     $('.catalog__main-information').eq(i).toggleClass('catalog__main-information_active');
     $('.catalog__additional-information').eq(i).toggleClass('catalog__additional-information_active');
   })
-}); 
-})
+});
+$('[data-form=consultation]').on('click', function() {
+$('.overlay, #consultation').fadeIn()
+});
+$('.modal__close').on('click', function(){
+$('.overlay, #order, #consultation, #thanks').fadeOut()
+});
+$('.catalog__button').each(function(i){
+  $(this).on('click', function(){
+    $('#order .modal__subtitle').text($('.catalog__header').eq(i).text());
+    $('.overlay, #order').fadeIn('slow');
+  });
+});
+$('input[name=phone]').mask("+380(99) 999-99-99");
+$('form').submit(function(e) {
+e.preventDefault();
+$.ajax({
+  type: "POST",
+  url: "mailer/smart.php",
+  data: $(this).serialize()
+}).done(function() {
+  $(this).find("input").val("");
+  $('form').trigger('reset');
+  $('#order, #consultation').fadeOut();
+  $('#thanks, .overlay').fadeIn('slow');
+});
+});
+$(window).scroll(function(){
+if ($(this).scrollTop() > 1200) {
+  $('.up').fadeIn();
+} else {
+  $('.up').fadeOut();
+}
+});
+$("a[href^='#']").click(function(){
+  const _href = $(this).attr("href");
+  $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+  return false;
+});
+});
 })(jQuery);
